41583ad0fc3996f1e80c3d47952704896cf459e5
rzartur <artur.rzycki@gmail.com>
2026-02-09 14:46:03 +0100

    Add exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "111222333" },
+  ]);
   const [newName, setNewName] = useState("Enter full name");
+  const [newPhone, setNewPhone] = useState("Enter phone number");
 
   /*const exists =
     persons.filter((person) => person.name === newName.trim()).length > 0;
@@ -11,12 +14,12 @@ const App = () => {
     (person) => person.name.toLowerCase() === newName.trim().toLowerCase(),
   );
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault();
-    console.log("empty");
+    //console.log("empty");
 
-    if (!newName.trim() || newName === "Enter full name") {
-      alert(`Field cant be blank`);
+    if (!newName.trim() || newName.trim() === "Enter full name") {
+      alert(`Name field cant be blank`);
       return;
     }
 
@@ -25,12 +28,19 @@ const App = () => {
       return;
     }
 
+    if (newPhone.trim() === "Enter phone number") {
+      alert(`Enter a phone number`);
+      return;
+    }
+
     const newPerson = {
       name: newName.trim(),
+      phone: newPhone.trim(),
     };
     //setPersons(persons.concat(newPerson));
     setPersons([...persons, newPerson]);
     setNewName("");
+    setNewPhone("");
   };
 
   const handleChangeName = (event) => {
@@ -38,23 +48,31 @@ const App = () => {
     setNewName(event.target.value);
   };
 
+  const handleChangePhone = (event) => {
+    setNewPhone(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleChangeName} />
+        </div>
+        <div>
+          number: <input value={newPhone} onChange={handleChangePhone} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <>
-        {persons.map((person) => (
-          <p key={person.name}>{person.name}</p>
-        ))}
-      </>
+
+      {persons.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.phone}
+        </p>
+      ))}
     </div>
   );
 };
