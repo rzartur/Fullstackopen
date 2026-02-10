ee9f3260f83963c3e253bb4e9405066c353b55e6
rzartur <artur.rzycki@gmail.com>
2026-02-10 19:47:22 +0100

    Add exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchFilter from "./components/SearchFilter";
 import NewPerson from "./components/NewPerson";
 import Numbers from "./components/Numbers";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   /*const exists =
     persons.filter((person) => person.name === newName.trim()).length > 0;
