4751ffc6675cdcc7774cd19e84d9e389685cb513
rzartur <artur.rzycki@gmail.com>
2026-02-15 16:47:46 +0100

    Add exercise 2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,12 +3,14 @@ import SearchFilter from "./components/SearchFilter";
 import NewPerson from "./components/NewPerson";
 import Numbers from "./components/Numbers";
 import personService from "./services/persons";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     personService.getAll().then((initialData) => {
@@ -58,6 +60,10 @@ const App = () => {
                 person.id === returnedPerson.id ? returnedPerson : person,
               ),
             );
+            setMessage(`Changed number to ${returnedPerson.number}`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
             setNewName("");
             setNewNumber("");
           });
@@ -81,6 +87,10 @@ const App = () => {
       setPersons([...persons, returnedPerson]);
       setNewName("");
       setNewNumber("");
+      setMessage(`Added ${returnedPerson.name}`);
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
     });
   };
 
@@ -126,6 +136,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <SearchFilter handleSearch={handleSearch} />
       <NewPerson
         addPerson={addPerson}
