9ce3cd42b4e3edfd3e8734a1432d28dba1832996
rzartur <artur.rzycki@gmail.com>
2026-02-17 22:10:28 +0100

    Added exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -35,6 +35,19 @@ app.get("/info", (request, response) => {
   );
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  console.log(person);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
