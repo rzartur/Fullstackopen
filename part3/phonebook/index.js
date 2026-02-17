025ebf414e61a539d2a076a31bde9a13940c315a
rzartur <artur.rzycki@gmail.com>
2026-02-17 22:22:35 +0100

    Add exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -46,6 +46,13 @@ app.get("/api/persons/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((n) => n.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
