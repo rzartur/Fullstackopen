d88a2b60f46baf43c2db622dbd22c74e1146adf5
rzartur <artur.rzycki@gmail.com>
2026-02-21 22:31:35 +0100

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-
 app.use(express.json());
+
+app.use(express.static("dist"));
 
 morgan.token("body", (request, response) => {
   return JSON.stringify(request.body);
@@ -117,7 +116,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
