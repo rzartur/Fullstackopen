4308980f3410c9ce7ac8b0ee91116963b837f740
rzartur <artur.rzycki@gmail.com>
2026-03-28 22:22:01 +0100

    Add exercise 4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -18,9 +18,11 @@ mongoose.connect(config.MONGODB_URI, { family: 4 }).then(() => {
 
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.getTokenFrom)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+
 app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -22,7 +22,7 @@ loginRouter.post('/', async (request, response) => {
     const token = jwt.sign(
         userForToken,
         process.env.SECRET,
-        { expiresIn: 30 }
+        { expiresIn: 60 }
     )
 
     response.status(200).send({ token, username: user.username, name: user.name })
